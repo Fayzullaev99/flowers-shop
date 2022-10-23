310ddbe9b3914dd4c2e131fbb9e55f489d5c49fe
Sanjar <youaxample@mail.com>
2022-10-23 19:19:33 +0500

    db.json created

---
+++ src/components/ProductCard/index.jsx
@@ -0,0 +1,24 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHeart as faHeartRegular } from '@fortawesome/free-regular-svg-icons'
+import { faHeart as faHeartSolid} from '@fortawesome/free-solid-svg-icons'
+import classNames from 'classnames'
+import React from 'react'
+import { useState } from 'react'
+import classes from './ProductCard.module.scss'
+
+function ProductCard({className,data}) {
+    const [image] = data.images
+    const [selected,setSelected] = useState(false)
+    const selectItem = ()=>setSelected((prevState)=>!prevState)
+  return (
+    <div className={classNames(classes['card'],className)}>
+        <img src={image} alt={data.name} className={classes['card__image']} />
+        <h3 className={classes['card__title']}>{data.name}</h3>
+        <p className={classes['card__price']}>{data.price} USD. <span className={classes['card__discount']}>{data.discount} USD.</span></p>
+        <button className={classNames(classes['card__button'],selected && classes['card__button_selected'])} onClick={selectItem} >{selected ? 'Added' : 'Add To Cart'}</button>
+        <button className={classes['card__like']}><FontAwesomeIcon icon={faHeartRegular}/></button>
+    </div>
+  )
+}
+
+export default ProductCard
--- src/pages/Category/index.jsx
+++ src/pages/Category/index.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
+import { useState } from 'react'
 import { useParams } from 'react-router-dom'
 import Header from '../../components/Header'
-
+import ProductCard from '../../components/ProductCard'
+import Container from '../../layout/Container'
+import classes from './Category.module.scss'
 function Category() {
-  const {productName} = useParams()
+  const { productName } = useParams()
+  const [data, setData] = useState(null)
+  const FetchData = () => {
+    fetch('http://localhost:4000/products')
+      .then(data => data.json())
+      .then(cards => setData(cards))
+  }
   return (
-    <Header />
+    <>
+      <Header />
+      <Container className={classes['cards']}>
+        {data && data.map((card) => (
+          <ProductCard key={card.id} className={classes['card-item']} data={card} />
+        ))}
+        <button onClick={FetchData}>FetchData</button>
+      </Container>
+    </>
   )
 }
 
