7dd8e73c9dea2865e96182b3715d16ef83feba4d
Sanjar <youaxample@mail.com>
2022-11-05 14:30:14 +0500

    finished validate form

--- src/components/OrderForm/index.jsx
+++ src/components/OrderForm/index.jsx
@@ -1,18 +1,28 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Container from "../../layout/Container";
 import classes from "./OrderForm.module.scss";
 import image from '../../images/OrderFormImage.jpeg'
+import classNames from 'classnames';
+import { validateName, validatePhoneContent, validatePhoneNumber, validateText } from './helper';
 
 const initialData = {
-  name:'',
-  tel:'',
-  text:''
+  name: '',
+  tel: '',
+  text: ''
 }
 
 function OrderForm() {
-  const [fields,setFields] = useState(initialData)
-  const handleChange = (e)=>setFields((prev)=>({...prev,[e.target.name]:e.target.value}))
-  const handleSubmit = (e)=>{
+  const [fields, setFields] = useState(initialData)
+  const [disabled, setDisabled] = useState(true)
+
+  useEffect(() => {
+    const isValid = validateName(fields.name) && validatePhoneNumber(fields.tel) && validateText(fields.text)
+    setDisabled(!isValid)
+  }, [fields])
+  const handleChange = (e) => {
+    if (e.target.name === 'tel' && validatePhoneContent(e.target.value)) return setFields((prev) => ({ ...prev, [e.target.name]: e.target.value }))
+  }
+  const handleSubmit = (e) => {
     e.preventDefault()
     setFields(initialData)
   }
@@ -24,7 +34,9 @@ function OrderForm() {
           <input className={classes['order-form__input']} type="text" placeholder='Name*' name='name' value={fields.name} onChange={handleChange} />
           <input className={classes['order-form__input']} type="tel" placeholder='Phone Number*' name='tel' value={fields.tel} onChange={handleChange} />
           <textarea className={classes['order-form__textarea']} placeholder='Your Ideas' name='text' value={fields.text} onChange={handleChange} />
-          <button className={classes['order-form__button']} onClick={handleSubmit} >Send</button>
+          <button className={classNames(classes['order-form__button'], {
+            [classes["order-form__button_disabled"]]: disabled
+          })} onClick={handleSubmit} disabled={disabled}>Send</button>
         </div>
         <img className={classes['order-form__image']} src={image} alt="flower" />
       </form>
